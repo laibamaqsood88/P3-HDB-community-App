6c088cc9fd20f7511d993f27833d02a0eb9acbec
nurleeyana <nurleeyana@users.noreply.github.com>
2026-04-15 21:58:53 +0800

    add ngrok url as allowedHost

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    allowedHosts: ['frigidity-musty-stratus.ngrok-free.dev']
+  },
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
